67ab00b6c1b00660f89b89f22608aba8a7ccdef7
joedupor <joedupor@gmail.com>
2023-08-29 11:01:09 -0500

    initial commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int favNum = 3;
+            Console.WriteLine("Try to guess my favorite number");
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNum )
+            {
+                Console.WriteLine("too low");
+            }
+
+           else if (userInput > favNum) 
+            {
+                Console.WriteLine("too high");
+            }
+
+           else 
+            {
+                Console.WriteLine("You're psychic!");
+            }
+
         }
     }
 }
