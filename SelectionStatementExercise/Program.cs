9590d38ea7b2506f19610140d746c3cbd0ed9f0a
joedupor <joedupor@gmail.com>
2023-08-29 11:35:29 -0500

    added fave subject survey

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -6,7 +6,7 @@
         {
 
             int favNum = 3;
-            Console.WriteLine("Try to guess my favorite number");
+            Console.WriteLine("Try to guess my favorite number!");
             int userInput = int.Parse(Console.ReadLine());
 
             if (userInput < favNum )
@@ -23,6 +23,39 @@
             {
                 Console.WriteLine("You're psychic!");
             }
+
+
+            Console.WriteLine("What is your favorite school subject? HISTORY, MATH, GYM, SCIENCE, or LANGUAGE? PLEASE use provided spelling and ALL CAPS when providing a response :)");
+            string userFavSubj = Console.ReadLine();
+
+            switch (userFavSubj)
+            {
+                case "HISTORY":
+                    Console.WriteLine("My favorite too!");
+                    break;
+
+                case "MATH":
+                    Console.WriteLine("Ah Math, the language of the cosmos!");
+                    break;
+
+                case "GYM":
+                    Console.WriteLine("Good to stay fit!");
+                    break;
+
+                case "SCIENCE":
+                    Console.WriteLine("Science, vital to humanity!");
+                    break;
+
+                case "LANGUAGE":
+                    Console.WriteLine("But of course!");
+                    break;
+
+                    default: 
+                    Console.WriteLine("Surely an interesting subject, but not a choice for this survey. Please try again!");
+                    break;   
+
+            }
+
 
         }
     }
